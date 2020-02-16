c09dc07fede32abe4941a5e7f97556596508b3d1
brainsparklabsapp <brainsparklabsapp@gmail.com>
2020-02-15 19:16:49 -0600

    Updates

---
+++ precache-manifest.fba39cd15b3289ab43366bb970c4ac2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5db149b1d16f5f483d23558fa5acd34e",
+    "url": "/app2020/index.html"
+  },
+  {
+    "revision": "38f74f2ab51d8bcba740",
+    "url": "/app2020/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6afd7205a7ba9aa08f0a",
+    "url": "/app2020/static/js/2.7eb86cf6.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/app2020/static/js/2.7eb86cf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38f74f2ab51d8bcba740",
+    "url": "/app2020/static/js/main.8f0116de.chunk.js"
+  },
+  {
+    "revision": "1679f4c08cd3c495a40d",
+    "url": "/app2020/static/js/runtime-main.7aec28e5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app2020/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0db44fef8e9a322b55aa42887826ee29.js"
+  "/app2020/precache-manifest.fba39cd15b3289ab43366bb970c4ac2f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/app2020/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
